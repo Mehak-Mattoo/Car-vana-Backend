37d26902d323936dfcfd194cfdf23087d69387e8
Mehak Mattoo <77096365+Mehak-Mattoo@users.noreply.github.com>
2024-11-14 14:24:01 +0530

    Delete Frontend directory

--- Frontend/src/components/Chip.js
+++
@@ -1,5 +0,0 @@
-import { Chip } from "@material-tailwind/react";
-
-export default function ChipComponent(props) {
-    return <Chip className="my-3 dark:text-white" variant="ghost" value={props.title} />;
-}
